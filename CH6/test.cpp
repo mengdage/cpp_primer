56b7631861245f5b8a9308728001c70cd6581543
meng <wulalameng@gmail.com>
2017-01-03 10:40:54 -0500

    function test

---
+++ CH6/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int &get(int * array, int index) { return array[index];}
+
+int main(){
+		int ia[10];
+		for(int i = 0; i != 10; ++i){
+				get(ia, i) = i;
+		}
+		for(int i = 0; i != 10; ++i){
+				std::cout << ia[i] << " ";
+		}
+		std::cout << std::endl;
+		return 0;
+}
